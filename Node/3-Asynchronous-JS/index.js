7d19a732592a76cef45c013955b31a7610cd4fb1
Akio1102 <cristiandaviddiazaza@gmail.com>
2023-08-31 22:32:15 -0500

    feat (3-Asynchronous-JS): 🚧 Inicio Video 5 Node
    Desarrollo hasta el 50:48 del 5 video Node

---
+++ Node/3-Asynchronous-JS/index.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("Not Found that file");
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("Not write that file");
+      resolve("success");
+    });
+  });
+};
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro("dog-img.txt", res.body.message);
+    console.log("Ramdon dog image saved to file");
+  } catch (error) {
+    console.log(error);
+    throw err;
+  }
+  return "2: Ready";
+};
+console.log("1: Will get dog pics!");
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log("3: Done getting dog pics!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+//     return writeFilePro("dog-img.txt", res.body.message);
+//   })
+//   .then(() => console.log("Ramdon dog image saved to file"))
+//   .catch((err) => console.log(err));
+
+/* fs.readFile((err, data) => {
+    if (err) console.log(err);
+
+    console.log(`Breed: ${data}`);
+
+    superagent
+        .get(`https://dog.ceo/api/breed/${data}/images/random`)
+        .then(res => {
+            console.log(res.body.message);
+
+            fs.writeFile("dog-img.txt", res.body.message, err => {
+                if (err) return console.log(err.message);
+                console.log("Ramdon dog image saved to file");
+            })
+        })
+        .catch(err => console.log(err))
+}) */
